e193a9f744e39919efcf3a3d34c44d3563cdb043
Mikhail Z <zaitsev@primetver.ru>
2014-11-10 10:43:50 +0400

    ON DELETE CASCADE

--- resources/server/postgres.sql
+++ resources/server/postgres.sql
@@ -12,7 +12,7 @@ CREATE TABLE messages (
   receiverId int,
   mess_text text,
   PRIMARY KEY (id),
-  FOREIGN KEY (queueId) REFERENCES queues (id),
+  FOREIGN KEY (queueId) REFERENCES queues (id) ON DELETE CASCADE,
   FOREIGN KEY (senderId) REFERENCES clients (id),
   FOREIGN KEY (receiverId) REFERENCES clients (id)
 );
