6a1907a917267588978f1d4122c8e27475db6d8c
Mikhail Z <zaitsev@primetver.ru>
2014-12-12 14:23:32 +0300

    modified:   resources/client/run.sh 	modified:   resources/server/run.sh

--- resources/client/run.sh
+++ resources/client/run.sh
@@ -1,2 +1,6 @@
 #java -agentlib:jdwp=transport=dt_socket,server=n,address=debian:15005,suspend=y -classpath "./libs/*:./config/" Client
-java -classpath "./libs/*:./config/" Client
+p=`echo $PWD`
+cd $HOME/client/
+java -classpath "./libs/*:./config/" Client &
+cd $p
+exit 0;
--- resources/server/run.sh
+++ resources/server/run.sh
@@ -1,2 +1,6 @@
 #java -agentlib:jdwp=transport=dt_socket,server=n,address=debian:17558,suspend=y -classpath "./libs/*:./config/" sr.Server
-java -classpath "./libs/*:./config/" sr.Server
+p=`echo $PWD`
+cd $HOME/server/
+java -classpath "./libs/*:./config/" sr.Server &
+cd $p
+exit 0;
